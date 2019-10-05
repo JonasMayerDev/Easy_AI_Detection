46df167791074cd3ea966950845c16b42a3f0b34
BySuxax <43114340+BySuxax@users.noreply.github.com>
2019-10-05 21:09:17 +0200

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -1,27 +1,24 @@
 import sys
-sys.path.append("/home/jonas/Dokumente/Easy_AI_Detection")
+sys.path.append("PATH THO THE Eeasy_AI_Detection.py file normaly in the repo folder")
 import Easy_AI_Detection as EAID
-#import numpy as np
-#from matplotlib import pyplot as plt
-#from matplotlib import cm
 from PIL import Image
 
-
+# Create instance
 E_AI_D = EAID.Easy_AI_Detection()
 
-(Boxes,img) = E_AI_D.classify_image("/home/jonas/Dokumente/Easy_AI_Detection/img0.png",True)
-(Boxes2,img2) = E_AI_D.classify_image("/home/jonas/Dokumente/Easy_AI_Detection/img1.png",True)
+#Classify Image 
+
+(Boxes,img) = E_AI_D.classify_image("PATH TO FILE OR CV2 IMAGE OR PIL IMAGE IF ITS A NUMPY IMAGE SET TO FALSE",True)
+
 
 #img = img[int(Boxes[0][1]),int(Boxes[0][3])][int(Boxes[0][0]):int(Boxes[0][2])]
 
 Image.fromarray(img).show()
-Image.fromarray(img2).show()
+
 
 #Boxes is an Array with [[left,top,right,bottom,class,score],[]]
 
 print(Boxes)
-
-print(Boxes2)
 
 
 
